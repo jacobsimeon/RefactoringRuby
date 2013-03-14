82f05f42b3f873baa0e18fadfc1555ad63d52610
Jacob Morris <jacob@creativesoapbox.com>
2013-03-14 12:10:09 -0600

    use own clas

--- src/equal_sum_substring.rb
+++ src/equal_sum_substring.rb
@@ -1,14 +1,18 @@
-class String
+class FindEqualSumSubstring
+  def initialize(target)
+    @target = target
+  end
+
   def equal_sum_substring
-    length.downto(1) do |limit|
-      (0..(length - limit)).each do |offset|
-        candidate = slice(offset, limit)
+    @target.length.downto(1) do |limit|
+      (0..(@target.length - limit)).each do |offset|
+        candidate = @target.slice(offset, limit)
         next unless candidate.length.even?
 
         half_length = candidate.length / 2
 
         first_half = candidate[0...half_length]
-        second_half = candidate[half_length...length]
+        second_half = candidate[half_length...@target.length]
 
         first_half_sum = first_half.each_char.inject(0) { |t, d| t += d.to_i }
         second_half_sum = second_half.each_char.inject(0) { |t, d| t += d.to_i }
